932a513d5471d0f6837e43bd76a88a3491c341bd
David Thuilot <dthuilot@hotmail.com>
2022-04-29 09:25:51 -0500

    setup json old value 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,7 @@ echo "json_file_path: $json_file_path"
 echo "json_field: $json_field"
 echo "json_value: $json_value"
 
-cat /github/workspace/$json_file_path
-
 # Extract current value
-json_old_value=cat /github/workspace/$json_file_path| jq ".[].$json_field"
+json_old_value=$(cat /github/workspace/$json_file_path| jq ".[].$json_field")
 
 echo "::set-output name=json_old_value::$json_old_value"
