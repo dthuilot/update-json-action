a20c3dd3e469ecdbe933f7590a138653cae79c0d
David Thuilot <dthuilot@hotmail.com>
2022-04-29 14:11:12 -0500

    new way to sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,16 @@ echo "json_new_value: $json_new_value"
 # Extract current value
 json_old_value=$(cat $json_file_path| jq ".[].$json_field")
 
-# Update the old value by the new one, sed delimiter must be replaced by #
+echo $json_old_value
 
-# sed -i '' "s#$json_old_value#$json_new_value#g" $json_file_path
-# sed -i '' 's/latest/oldest/g' /github/workspace/data.json
+# sed goal
+# sed -i '' 's/"399252381561.dkr.ecr.us-east-1.amazonaws.com/api:latest"/"nginx:latest"/g' $json_file_path
 
-echo "Tout roule !" >  /github/workspace/banzai.json
+# Normal sed command
+# sed -i '' 's/$json_new_value/$json_old_value/g' $json_file_path
+
+# Normal sed command with updated space as separator
+sed 's $json_new_value $json_old_value g' $json_file_path 
 
 
 echo "::set-output name=json_old_value::$json_old_value"
