c3f69de87531b05185089694b77ad6d63b6e9af3
David Thuilot <dthuilot@hotmail.com>
2022-04-29 07:58:55 -0500

    Implement jq to find current value

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh -l
 
-echo "json_field $1"
-echo "json_value $2"
-json_old_value=$(date)
+json_file_path=$1
+json_field=$2
+json_value=$3
+
+#echo "json_file_path $1"
+#echo "json_field $2"
+#echo "json_value $3"
+
+# Extract current value
+json_old_value= cat $json_file_path| jq '.[].images'
+
+#json_old_value=$(date)
+
 echo "::set-output name=json_old_value::$json_old_value"
