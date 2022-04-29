9bd0abc3aafd2a17dffff505d7eb8baa23ece355
David Thuilot <dthuilot@hotmail.com>
2022-04-29 08:02:42 -0500

    Fix jq command line

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,9 @@ json_value=$3
 #echo "json_value $3"
 
 # Extract current value
-json_old_value= cat $json_file_path| jq '.[].images'
+cat $1| jq '.[].images'
 
+#json_old_value= cat $json_file_path| jq '.[].images'
 #json_old_value=$(date)
 
 echo "::set-output name=json_old_value::$json_old_value"
