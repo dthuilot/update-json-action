88400982e25964f940d8b60f143578a3c90e4b3f
David Thuilot <dthuilot@hotmail.com>
2022-04-29 10:04:46 -0500

    setup sed 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo "json_new_value: $json_new_value"
 # Extract current value
 json_old_value=$(cat /github/workspace/$json_file_path| jq ".[].$json_field")
 
-# Update the old value by the new one
-sed -i "s/$json_old_value/$json_new_value/g" /github/workspace/$json_file_path
+# Update the old value by the new one, sed delimiter must be replaced by #
+sed -i '' "s#$json_old_value#$json_new_value#g" /github/workspace/$json_file_path
 
 echo "::set-output name=json_old_value::$json_old_value"
