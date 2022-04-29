ee16b9e97635c32ae5ffe576d5cef692aa4f4bd6
David Thuilot <dthuilot@hotmail.com>
2022-04-29 10:19:51 -0500

    Update json_file_patch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-json_file_path=$1
+json_file_path="/github/workspace/$1"
 json_field=$2
 json_new_value=$3
 
@@ -9,9 +9,9 @@ echo "json_field: $json_field"
 echo "json_new_value: $json_new_value"
 
 # Extract current value
-json_old_value=$(cat /github/workspace/$json_file_path| jq ".[].$json_field")
+json_old_value=$(cat $json_file_path| jq ".[].$json_field")
 
 # Update the old value by the new one, sed delimiter must be replaced by #
-sed -i '' "s#$json_old_value#$json_new_value#g" /github/workspace/$json_file_path
+# sed -i '' "s#$json_old_value#$json_new_value#g" $json_file_path
 
 echo "::set-output name=json_old_value::$json_old_value"
